5e4321aa12e8ba5fda86851d62c641e124864cbb
DaCHillZone <christoffer.hill@students.first.edu>
2023-02-24 21:49:58 -0500

    5

--- practice-js/js/app.js
+++ practice-js/js/app.js
@@ -4,4 +4,8 @@ console.log("AYO")
 // Alert
 alert("HELP!!!")
 
-document.getElementById("demo").style.color = "red";
+// document.getElementById("demo").style.color = "red";
+
+let demo = document.getElementById("demo");
+demo.style.color = "red";
+
