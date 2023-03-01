6050a6de8e9bccb3b71aca44519c3359538a2e63
DaCHillZone <christoffer.hill@students.first.edu>
2023-02-28 21:42:54 -0500

    00

---
+++ js-countdown/js/app.js
@@ -0,0 +1,60 @@
+// console
+// console.log("AYO")
+
+// Alert
+// alert("HELP!!!")
+
+// document.getElementById("demo").style.color = "red";
+
+// let demo = document.getElementById("demo");
+// demo.style.color = "red";
+
+// demo.style.margin ="12px";
+// demo.style.textAlign ="center";
+
+
+// demo.innerHTML = "I'm Now Corupt!!! Battle Me ORGANIC!!!!!!!";
+
+
+// Write a funtion to greet me
+// Function Decoration
+
+// function greet() {
+//     alert("Hello!");
+// }
+
+// Square A Number
+// function square(number) {
+//     return number * number;
+// }
+ 
+// save value in html
+// let result = square(number);
+// document.getElementById("demo").
+// innerHTMLb = result;
+
+ 
+
+
+
+function plusFive(num) {
+    return num + 5;
+    // fill in the blank
+}
+
+// Increase winning number by 5
+function increaseLargerNumber(x, y) {
+    // save varable for result
+    let larger;
+    if(x >= y) {
+        larger = x;
+    } else {
+        larger = y;
+        // return larger number plus five
+        
+    }
+    return plusFive (larger);
+}
+
+
+console.log(increaseLargerNumber(35, 34));
