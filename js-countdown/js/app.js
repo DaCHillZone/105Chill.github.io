9b57a3a277b87a60d2526b1a8d996b6c4cea9014
DaCHillZone <christoffer.hill@students.first.edu>
2023-02-28 22:42:01 -0500

    010

--- js-countdown/js/app.js
+++ js-countdown/js/app.js
@@ -1,1 +1,20 @@
-let deadline 
+let deadline = new Date("November 28, 2022 13:00:00");
+
+let countdown = document.getElementById("countdown");
+countdown.innerHTML = "";
+
+function setCountdown () {
+    let now = new Date();
+
+    let timeLeft = deadline.getTime() - now.getTime();
+
+    let daysLeft = convertToDaysLeft(timeLeft);
+    let hoursLeft = convertToHoursLeft(timeLeft);
+    let minutesLeft = convertToMinutesLeft(timeLeft);
+    let secondsLeft = convertToSecondsLeft(timeLeft);
+
+    countdown.innerHTML =
+        formatCountdownText(daysLeft, hoursLeft, minutesLeft, secondsLeft);
+}
+
+setInterval(setCountdown, 1000);
