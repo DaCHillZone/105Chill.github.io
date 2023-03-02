ba483b0888339a69fec64cdae70e0e09d48b69e3
DaCHillZone <christoffer.hill@students.first.edu>
2023-03-01 21:33:53 -0500

    calc-2

--- js-calc/js/app.js
+++ js-calc/js/app.js
@@ -1,1 +1,15 @@
- 
+//  console.log("hi");
+
+let answer = document.getElementById("answer");
+
+function twentyTwoPlusTwo() {
+    answer.innerHTML = 22 + 2;
+}
+
+function eightySevenTimesEight() {
+    answer.innerHTML = 87 * 8;
+}
+
+function twentyFiveDivideByOnehundred() {
+    answer.innerHTML = 25 / 100;
+}
