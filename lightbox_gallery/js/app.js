3badcaea6ec9e070f8a05867e0eaf7b6c2a69fdf
DaCHillZone <christoffer.hill@students.first.edu>
2023-03-03 21:14:14 -0500

    gal-2

--- lightbox_gallery/js/app.js
+++ lightbox_gallery/js/app.js
@@ -9,24 +9,36 @@ function openLightbox() {
 }
 
 // image varable
-let coffeeImage = document.getElementById("coffeeImage");
-coffeeImage.onclick = openLightbox;
+// let coffeeImage = document.getElementById("coffeeImage");
+// coffeeImage.onclick = openLightbox;
 
-let coffeeImage2 = document.getElementById("coffeeImage2");
-coffeeImage2.onclick = openLightbox;
-
-
-let coffeeImage3 = document.getElementById("coffeeImage3");
-coffeeImage3.onclick = openLightbox;
-
-let coffeeImage4 = document.getElementById("coffeeImage4");
-coffeeImage4.onclick = openLightbox;
+// let coffeeImage2 = document.getElementById("coffeeImage2");
+// coffeeImage2.onclick = openLightbox;
 
 
+// let coffeeImage3 = document.getElementById("coffeeImage3");
+// coffeeImage3.onclick = openLightbox;
+
+// let coffeeImage4 = document.getElementById("coffeeImage4");
+// coffeeImage4.onclick = openLightbox;
+
+// image group
+let galleryImages = document.getElementsByClassName("galleryImage");
+
+// making a clickable image loop
+
+for(let i = 0; i < galleryImages.length; i++) {
+    let galleryImage = galleryImages[i];
+    galleryImage.onclick = openLightbox; 
+} 
+
+
+
+// close
 function closeLightbox() {
     lightboxContainer.classList.remove("display");
     lightbox.src = "";
-}
+} 
 
 lightboxBackground.onclick = closeLightbox;
 lightboxCloser.onclick = closeLightbox;
