6a7b57c6b14c485dd89659cfbe0fd8847013acfc
DaCHillZone <christoffer.hill@students.first.edu>
2023-03-08 22:11:19 -0500

    gsap8

--- gsap1/js/app.js
+++ gsap1/js/app.js
@@ -1,3 +1,5 @@
+
+// box animation
 gsap.from(".chucked",{
     duration: 5,
     ease: "bounce",
@@ -5,4 +7,12 @@ gsap.from(".chucked",{
     x:-500,
     y:-500,
     stagger: 0.5,
+});
+
+// text animation
+gsap.from(".headline",{
+    duration: 2.5,
+    opacity: 0,
+    y: 300,
+    stagger:0.3,
 });
