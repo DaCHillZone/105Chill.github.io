d13cbb2a38b80fa677b3241f10ffd5a237d82b8a
DaCHillZone <christoffer.hill@students.first.edu>
2023-03-08 22:35:22 -0500

    gsap2-0

---
+++ gsap2/js/app.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// box animation
+// gsap.from(".chucked",{
+//     duration: 5,
+//     ease: "bounce",
+//     rotation: 720,
+//     x:-500,
+//     y:-500,
+//     stagger: 0.5,
+// });
+
+// text animation
+// gsap.from(".headline",{
+//     duration: 2.5,
+//     opacity: 0,
+//     y: 300,
+//     stagger:0.3,
+// });
