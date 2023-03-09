031c4bbc562a864698a02d54e4eab847c11cddf5
DaCHillZone <christoffer.hill@students.first.edu>
2023-03-08 22:57:38 -0500

    gsap2-4

--- gsap2/js/gear.js
+++ gsap2/js/gear.js
@@ -2,11 +2,15 @@
         // The element is selected, add some properties to this GSAP method to rotate the gear infinitely. 
         gsap.from("#gear_outer",{
             transformOrigin:"50% 50%",
-            duration: 2,
-            rotation: -4400
+            duration: 20,
+            rotation: 360,
+            repeat: -1,
+            ease:"none",
         });
 
         // Make a gsap method to have the title text start from above the clipping circle.
+
+        gsap.from("");
 
         // Here, make a gsap method that sets each line on the bridge to start from underneath the clipping circle and transition to where it must go.
         // Note, you'll likely need to add a class to those lines in order to select them with GSAP effectively.
